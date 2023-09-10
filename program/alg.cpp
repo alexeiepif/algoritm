0b663f3066e4fd1ad6f766581992d331469f95b3
alexeiepif <aled2093746@gmail.com>
2023-09-10 15:54:04 +0300

    program

---
+++ program/alg.cpp
@@ -0,0 +1,15 @@
+#include <iostream> 
+#include <math.h> 
+using namespace std; 
+int func(int n)
+{
+    if(n==1)
+        return n;
+    else
+        return n-1+func(n-1);
+}
+int main()
+{
+    int a = func(2024)-func(2022);
+    cout << a << endl;
+} 
